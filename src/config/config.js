5919bfbe164b048ea9710fc5d6db22fd15868b40
asvessmii <svetlichnaya.alica@yandex.ru>
2025-06-04 19:15:06 +0300

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
+const defaultDbUrl = process.env.VERCEL
+  ? 'sqlite:///tmp/database.sqlite'
+  : 'sqlite://./database.sqlite';
+
 module.exports = {
   botToken: process.env.BOT_TOKEN,
   adminId: process.env.ADMIN_USER_ID,
-  dbUrl: process.env.DB_URL || 'sqlite://./database.sqlite',
+  dbUrl: process.env.DB_URL || defaultDbUrl,
   webhookUrl: process.env.WEBHOOK_URL
 };
